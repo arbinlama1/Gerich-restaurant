7f1efbc997219de59d68595f8c4ffcdf3ad7a2cf
arbinlama1 <tamangarbin442@gmail.com>
2025-12-07 19:24:15 +0545

    Fixing the error in ui

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import images from '../constrant/images';
 const Navbar = () => {
-    const[toggleMenu, setToggleMenu] = useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
         <nav className='app__navbar'>
             <div className="app__navbar-logo">
@@ -22,21 +22,21 @@ const Navbar = () => {
                 <a href="/" className='p__opensans'>Book Table</a>
             </div>
             <div className="app__navbar-smallscreen">
-                <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => { setToggleMenu(true)}} />
+                <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => { setToggleMenu(true) }} />
                 {toggleMenu &&
-                <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-                    <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => { setToggleMenu(false)}} />
-                    <ul className='app__navbar-smallscreen-links'>
-                        <li className='p__opensans'><a href='#home'>Home</a></li>
-                        <li className='p__opensans'><a href='#about'>About</a></li>
-                        <li className='p__opensans'><a href='#menu'>Menu</a></li>
-                        <li className='p__opensans'><a href='#award'>Awards</a></li>
-                    </ul>
-                </div>
-}
+                    <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+                        <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => { setToggleMenu(false) }} />
+                        <ul className='app__navbar-smallscreen-links'>
+                            <li className='p__opensans'><a href='#home'>Home</a></li>
+                            <li className='p__opensans'><a href='#about'>About</a></li>
+                            <li className='p__opensans'><a href='#menu'>Menu</a></li>
+                            <li className='p__opensans'><a href='#award'>Awards</a></li>
+                        </ul>
+                    </div>
+                }
             </div>
         </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
